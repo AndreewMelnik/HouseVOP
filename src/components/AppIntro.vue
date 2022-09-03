da28b0fd1d17e5a03695747ebf0c639f6bc6e465
AndreewMelnik <andreew.mel@gmail.com>
2022-09-03 16:43:50 +0300

    /

--- src/components/AppIntro.vue
+++ src/components/AppIntro.vue
@@ -3,7 +3,7 @@
   <div class="intro__wrapper">
     <div class="intro__title">Жилой комплекс <br/>в историческом центре</div>
     <a class="intro__logo" >
-      <img class="intro__logo-img" src="../assets/images/mouse.svg" alt="mouse" />
+<!--      <img class="intro__logo-img" src="../assets/images/mouse.svg" alt="mouse" />-->
     </a>
     <div class="intro__contacts">
         <div class="intro__contacts-caption">
@@ -48,13 +48,26 @@ export default {
 }
 .intro__logo{
   margin-top: 100px;
+  display: inline-block;
+  padding-bottom: 40px;
+  background: url("../assets/images/mouse.svg") no-repeat 95% center;
+  transition: all 0.2s;
+  width: 40px;
+  height: 40px;
+}
+
+.intro__logo:hover, .intro__logo:focus {
+  background: url("../assets/images/mouse.svg") no-repeat bottom center;
 }
 .intro__contacts{
   display: flex;
   flex-wrap: wrap;
-  margin-top: 200px;
+  margin-top: 160px;
   width: 582px;
   justify-content: space-around;
+}
+.intro__contacts-caption{
+  display: flex;
 }
 .intro__contacts-title{
   font-family: 'Raleway';
@@ -62,15 +75,19 @@ export default {
   line-height: 21px;
 }
 .intro__contacts-geo{
-  color: #fff;
-  /*padding-right: 25px;*/
   background: url(../assets/images/geo.svg) no-repeat right;
   background-size: 14px;
-  /*margin-right: 14px;*/
+  width: 15px;
+  height: 21px;
+  margin-right: 18px;
+
 }
 .intro__contacts-tel{
   background: url(../assets/images/tel.svg) no-repeat right;
   background-size: 14px;
+  width: 15px;
+  height: 21px;
+  margin-right: 18px;
 }
 
 </style>
